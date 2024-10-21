09614cec616b30d6153eecc136b69855d40013a4
1darko <dakojic@student.42.fr>
2024-10-21 12:24:17 +0200

    echo normine

--- minishell/builtin/ft_echo.c
+++ minishell/builtin/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: dakojic <dakojic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 12:40:04 by dakojic           #+#    #+#             */
-/*   Updated: 2024/10/20 17:39:23 by dakojic          ###   ########.fr       */
+/*   Updated: 2024/10/21 12:00:48 by dakojic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,62 +15,62 @@
 // Takes pointer to execcmd struct as arg since its the end product and frees it
 // Echo always returns 0 unless write fails
 
-
-static int n_option(char **cmd, int *i, bool *arg)
+static int	n_option(char **cmd, int *i, bool *arg)
 {
-    int j;
+	int	j;
 
-    j = 2;
-    while(cmd[*i] && cmd[*i][0]== '-' && cmd[*i][1] == 'n')
-    {
-        while(cmd[*i][j] == 'n')
-            j++;
-        if(cmd[*i][j] == '\0')
-            *arg = true;
-        else
-            return (0);
-        (*i)++;
-        j = 2;
-    }
-    return (0);
+	j = 2;
+	while (cmd[*i] && cmd[*i][0] == '-' && cmd[*i][1] == 'n')
+	{
+		while (cmd[*i][j] == 'n')
+			j++;
+		if (cmd[*i][j] == '\0')
+			*arg = true;
+		else
+			return (0);
+		(*i)++;
+		j = 2;
+	}
+	return (0);
 }
 
-int ft_echo(t_execs *execs)
+int	ft_echo(t_execs *execs)
 {
-    int i;
-    bool n_arg;
+	int			i;
+	bool		n_arg;
 	t_execcmd	*cmd;
 
 	cmd = (t_execcmd *)execs->cmd;
-    i = 1;
-    n_arg = false;
-    n_option(cmd->args, &i, &n_arg);
-    while(cmd->args[i])
-    {
-        write(1, cmd->args[i], ft_strlen(cmd->args[i]));
-        if(cmd->args[i + 1] != 0)
-            write(1, " ", 1);
-        i++;
-    }
-    if(n_arg == false)
-        write(1, "\n", 1);
-    i = 0;
+	i = 1;
+	n_arg = false;
+	n_option(cmd->args, &i, &n_arg);
+	while (cmd->args[i])
+	{
+		write(1, cmd->args[i], ft_strlen(cmd->args[i]));
+		if (cmd->args[i + 1] != 0)
+			write(1, " ", 1);
+		i++;
+	}
+	if (n_arg == false)
+		write(1, "\n", 1);
+	i = 0;
 	arrayfree(cmd->args);
 	cmd->args = 0;
-    return (0);
+	return (0);
 }
 
 // int main()
 // {
 //     char **cmd = malloc(5 * sizeof(char *));
-//     if (!cmd) return 1;
+//     if (!cmd) return (1);
 
-//     cmd[0] = strdup("echo"); // Use strdup to duplicate string literals safely
+//     cmd[0] = strdup("echo");
+	// Use strdup to duplicate string literals safely
 //     cmd[1] = strdup("-nABCn");
 //     cmd[2] = strdup("-n");
 //     cmd[3] = strdup("World");
 //     cmd[4] = NULL;
 //     ft_echo(&cmd);
 //     // ft_echo(&cmd);
-//     return 0;
+//     return (0);
 // }
