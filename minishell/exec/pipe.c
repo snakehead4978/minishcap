0d6a310f81802683361ab33e5a050dd3368c0944
snek <jeremie.hk@gmail.com>
2024-11-28 21:26:41 +0100

    pipe problems

--- minishell/exec/pipe.c
+++ minishell/exec/pipe.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipe.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dakojic <dakojic@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jla-chon <jla-chon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 22:11:04 by snek              #+#    #+#             */
-/*   Updated: 2024/11/28 19:52:48 by dakojic          ###   ########.fr       */
+/*   Updated: 2024/11/28 21:24:23 by jla-chon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,18 @@ static void	catcher_sigquit(int signum)
 		g_bigsignal = SIGQUIT;
 }
 
-static void	ft_pipe2(t_execs *exec, int fd[3], t_pipecmd *cmds, t_list *fds)
+static void	ft_pipe2(t_execs *exec, int fd[3], t_pipecmd *cmds, char *buff)
 {
 	t_fds	*tmp;
 	t_shell	*shell;
-	char	*buff;
 	int		err;
+	t_list	*fds;
 
+	fds = listnew(fdsnew(0, 0), fdsfree);
 	tmp = fds->data;
 	signal(SIGQUIT, catcher_sigquit);
 	close(fd[0]);
 	shell = exec->shell;
-	buff = exec->buff;
 	tmp->fd = fd[1];
 	tmp->type = FD_FILEOUT;
 	if (!listaddback(&exec->fds, fds, fdsfree))
@@ -37,6 +37,7 @@ static void	ft_pipe2(t_execs *exec, int fd[3], t_pipecmd *cmds, t_list *fds)
 	if (ft_expandcmd(exec, cmds->left))
 		exit_execfree(exec, 1);
 	err = ft_sorter(exec, cmds->left);
+	ft_removefd(fds);
 	if (err != 1)
 		execfree(exec);
 	free(buff);
@@ -45,30 +46,81 @@ static void	ft_pipe2(t_execs *exec, int fd[3], t_pipecmd *cmds, t_list *fds)
 	exit(err);
 }
 
+static void	ft_pipe3(t_execs *exec, int fd[3], t_pipecmd *cmds)
+{
+	t_fds	*tmp;
+	t_shell	*shell;
+	char	*buff;
+	int		err;
+	t_list	*fds;
+
+	signal(SIGQUIT, catcher_sigquit);
+	fds = listnew(fdsnew(0, 0), fdsfree);
+	tmp = fds->data;
+	shell = exec->shell;
+	buff = exec->buff;
+	tmp->fd = fd[0];
+	tmp->type = FD_FILEIN;
+	if (!listaddback(&exec->fds, fds, fdsfree))
+		exit_execfree(exec, 1);
+	if (ft_expandcmd(exec, cmds->right))
+		exit_execfree(exec, 1);
+	err = ft_sorter(exec, cmds->right);
+	ft_removefd(fds);
+	if (err != 1)
+		execfree(exec);
+	free(buff);
+	arrayfree(&shell->env);
+	free(shell);
+	exit(err);
+}
+
+static int	ispipe(t_execs *exec, t_cmd *cmds)
+{
+	int	type;
+
+	type = cmds->type;
+	if (type == PIPE)
+		return (1);
+	else if (type == REDIR || type == HERE)
+		return (ispipe(exec, ((t_redircmd *)cmds)->cmd));
+	return (0);
+}
+
 int	ft_pipe(t_execs *exec, int err, t_pipecmd *cmds)
 {
 	int			fd[3];
-	t_list		*fds;
-	t_fds		*tmp;
 
 	if (!exec->cmd)
 		return (0);
 	pipe(fd);
-	fds = listnew(fdsnew(0, 0), fdsfree);
-	tmp = fds->data;
+	printf("FORK 1\n");
 	fd[2] = fork();
 	if (!fd[2])
-		ft_pipe2(exec, fd, cmds, fds);
+		ft_pipe2(exec, fd, cmds, exec->buff);
 	else
 	{
 		close(fd[1]);
-		tmp->fd = fd[0];
-		tmp->type = FD_FILEIN;
-		if (!listaddback(&exec->fds, fds, fdsfree))
-			return (execfree(exec), 1);
-		if (ft_expandcmd(exec, cmds->right))
-			return (execfree(exec), 1);
-		err = ft_sorter(exec, cmds->right);
+		if (ispipe(exec, ((t_pipecmd *)cmds)->right))
+		{
+			ft_pipe3(exec, fd, cmds);
+		}
+		printf("FORK 2\n");
+		fd[2] = fork();
+		if (!fd[2])
+			ft_pipe3(exec, fd, cmds);
 	}
-	return (close(fd[0]), waitpid(fd[2], &fd[2], 0), ft_removefd(fds), err);
+	signal(SIGQUIT, SIG_IGN);
+	close(fd[0]);
+	waitpid(fd[2], &err, 0);
+	if (WIFSIGNALED(err) && WTERMSIG(err) == SIGINT)
+		g_bigsignal = SIGINT;
+	if (WIFEXITED(err))
+		err = WEXITSTATUS(err);
+	if (err == 1)
+		err = 333;
+	if (err == 131 && g_bigsignal != SIGQUIT)
+		write(2, "Quit (core dumped)\n", 19);
+	printf("I exited with err %d and %d:\n", err, g_bigsignal);
+	return (err);
 }
